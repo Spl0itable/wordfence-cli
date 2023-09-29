e3ece15c5229355b6c1d32af6798720ba11d07ed
LA <135403604+Spl0itable@users.noreply.github.com>
2023-09-29 12:31:54 +0930

    Update progress.py

--- wordfence/cli/scan/progress.py
+++ wordfence/cli/scan/progress.py
@@ -527,7 +527,7 @@ class ProgressDisplay:
     def _setup_colors(self) -> None:
         curses.start_color()
 
-        curses.init_pair(CYAN_TEXT, curses.COLOR_CYAN, curses.COLOR_BLACK)
+        curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
         self.color_brand = curses.color_pair(1)
 
     def clear(self):
